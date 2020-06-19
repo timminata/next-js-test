bd0a24030f39e07a82c9f8c596bbb90cf6e3b3ab
Tim Trewartha <timt@whereismytransport.com>
2020-06-19 21:55:01 +0200

    ok

--- pages/lines/[id].js
+++ pages/lines/[id].js
@@ -1,98 +1,98 @@
-import { useRouter } from 'next/router'
-import fetch from 'node-fetch'
-import axios from 'axios';
-import * as querystring from 'querystring';
-import Head from 'next/head';
+import { useRouter } from "next/router";
+import fetch from "node-fetch";
+import Head from "next/head";
+import {getTapiToken, getTapiLine} from '../../lib/helpers'
 
 let tapiToken = null;
 
-async function getTapiToken() {
-    console.log('getTapiToken');
-    const body = querystring.stringify({
-        client_id: '531434b5-c748-47db-8023-ff88718e5228',
-        client_secret: '8CUUBAcIcZYvksBo4KP7orFsv+iYXbsve2h/n/13JK0=',
-        grant_type: 'client_credentials',
-        scope: 'transportapi:all transportapi:linesshape:read transportapi:linesgeometry:read',
-      });
-      const config = {
-        headers: {
-          'content-type': 'application/x-www-form-urlencoded',
-        },
-      };
-
-    const result = await axios
-        .post('https://identity.whereismytransport.com/connect/token', body, config);
-        const token = result.data.access_token;
-        tapiToken = token;
-        return token;
-}
-
 const dictionary = {};
 
-async function getTapiLineName(lineId) {
-  console.log('getTapiLineName');
-  const headers = {
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${tapiToken}`,
-    },
-  };
-  const result = await axios
-    .get(`https://platform.whereismytransport.com/api/lines/${lineId}`, headers)
-
-  const line = result.data;
-  dictionary[lineId] = line;
-  return line;
-}
-
 const Line = (props) => {
-  const router = useRouter()
-  const { id, pid } = router.query
+  const router = useRouter();
+  const { id, pid } = router.query;
+  const line = props.line;
+  const lineColour = line.colour;
 
   return (
-
-    <>
-        <Head>
-        <title>{props.line.name.substring(0,65)}</title>
+    <div className="container">
+      <Head>
+        <title>{props.line.name.substring(0, 65)}</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="Your one stop shop for transport data!"></meta>
+        <meta
+          name="description"
+          content="Your one stop shop for transport data!"
+        ></meta>
         <meta property="og:title" content="TAPI Data" />
-        <meta property="og:url" content={`http://trewartha.za.net/lines/${id}`} />
+        <meta
+          property="og:url"
+          content={`http://trewartha.za.net/lines/${id}`}
+        />
         <meta property="og:description" content="This is meow"></meta>
-        <meta property="og:image" content="https://pbs.twimg.com/profile_images/765843200691019776/WB3R_p3__400x400.jpg"></meta>
+        <meta
+          property="og:image"
+          content="https://pbs.twimg.com/profile_images/765843200691019776/WB3R_p3__400x400.jpg"
+        ></meta>
       </Head>
-    <p>TAPI Line: {id}</p>
-    <p>TAPI Line Name: {props.line.name}</p>
-    <p>Device ID: {pid}</p>
-    <p>The time is: {props.timeData.datetime}</p>
-    </>
-  )
-}
+      <div className="card">
+        <h2>{line.agency.name}</h2>
+        <p><b>ID:</b> {id}</p>
+        <p><b>Name:</b> {props.line.name}</p>
+        <p><b>Shortname:</b> {props.line.shortName}</p>
+        <p><b>Mode:</b> {props.line.mode}</p>
+        <p><b>Device ID:</b> {pid}</p>
+        <p><b>Time:</b> {props.timeData.datetime}</p>
+      </div>
+
+      <div className="footer"></div>
+
+      <style jsx>{`
+         h2 {
+           text-align: center;
+         }
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        .card {
+          padding: 1rem;
+          border: 3px solid #4F5457;
+          border-radius: 10px;
+          background: ${lineColour};
+        }
+      `}</style>
+    </div>
+  );
+};
 
 export async function getServerSideProps(context) {
-    const url = 'http://worldtimeapi.org/api/timezone/Africa/Johannesburg';
-    const result = await fetch(url);
-    const timeData = await result.json();
-    const lineId = context.query.id;
+  const url = "http://worldtimeapi.org/api/timezone/Africa/Johannesburg";
+  const result = await fetch(url);
+  const timeData = await result.json();
+  const lineId = context.query.id;
 
-    if (tapiToken == null) {
-        await getTapiToken();
-    }
-    else {
-        console.log("has valid token");
-    }
-    if (!dictionary[lineId]) {
-        await getTapiLineName(lineId);
-    } else {
-        console.log("cache hit");
-    }
-    //const line = await getTapiLineName('T9htbUaZakysEKu4AXlQeg');
-    const line = dictionary[lineId];
-    
-    return {
-      props: { timeData: timeData, line: line } // will be passed to the page component as props
-    }
+  if (tapiToken == null) {
+    const token = await getTapiToken();
+    tapiToken = token;
+  } else {
+    console.log("has valid token");
   }
+  if (!dictionary[lineId]) {
+    const line = await getTapiLine(lineId);
+    dictionary[lineId] = line;
+  } else {
+    console.log("cache hit");
+  }
+  //const line = await getTapiLineName('T9htbUaZakysEKu4AXlQeg');
+  const line = dictionary[lineId];
 
-export default Line
+  return {
+    props: { timeData: timeData, line: line }, // will be passed to the page component as props
+  };
+}
+
+export default Line;
